8a9ee8e3715c6b035ca9e195521916ba99d5bf6e
Aishwary Gupta <aish042004@gmail.com>
2025-07-07 22:29:44 +0530

    gsap start

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,7 @@
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
 
+
   return (
     <main>
       <h1>GsapTo</h1>
@@ -30,7 +31,7 @@ const GsapTo = () => {
       </p>
 
       <div className="mt-20">
-        <div id="blue-box" className="w-20 h-20 bg-blue-500 rounded-lg" />
+        <div id="blue-box" className="w-20 h-20 bg-red-500 rounded-lg" />
       </div>
     </main>
   );
